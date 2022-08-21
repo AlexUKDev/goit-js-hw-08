9e94203a47766d2cf5295d8faa88bb071fedde1c
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-08-21 23:59:27 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -33,22 +33,33 @@ function onFormSubmit(e) {
   console.log("Форма успешно отправленна!")
   e.currentTarget.reset();
   console.log("Вот что мне удалось собрать от юзера", accData)
+  cleanLocalStorage();
+  console.log("Локас стораЖ был очищен")
 }
 
 function onInputChange(e) {
   let userEmail = e.target.value;
   console.log(userEmail)
   accData.email = userEmail;
+  toLocalStorageSave()
 }
 
 function onTextareaChange(e) {
   let userMassage = e.target.value;
   console.log(userMassage)
   accData.message = userMassage;
+  toLocalStorageSave()
 }
 
 console.log(accData)
 
-const toStrData = JSON.stringify(accData);
 
-localStorage.setItem(KEY_LOCSTOR, toStrData)
+function toLocalStorageSave(){
+  const toStrData = JSON.stringify(accData);
+  console.log(toStrData);
+  localStorage.setItem(KEY_LOCSTOR, toStrData)
+}
+
+function cleanLocalStorage(){
+  localStorage.removeItem(KEY_LOCSTOR);
+}
